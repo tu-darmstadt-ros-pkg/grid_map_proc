e44a8fc943de07f7b9984a2fadcfb43ffc059a30
Kevin_Daun <daun@sim.informatik.tu-darmstadt.de>
2023-11-02 13:10:18 +0100

    Fix dist_trans_layer argument forwarding.

--- src/grid_map_transforms.cpp
+++ src/grid_map_transforms.cpp
@@ -203,8 +203,7 @@ namespace grid_map_transforms{
                                   frontier_cells,
                                   min_frontier_dist_,
                                   min_frontier_size,
-                                  occupancy_layer,
-                                  dist_trans_layer);
+                                  occupancy_layer);
 
     for (size_t i = 0; i < obstacle_cells.size(); ++i){
       const grid_map::Index& point = obstacle_cells[i];
